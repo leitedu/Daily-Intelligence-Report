8312758c22ce6a326da0a0de9d519a3802b1617d
Eduardo Leite <133655512+leitedu@users.noreply.github.com>
2026-07-23 18:19:53 -0300

    Update transform.py

--- transform.py
+++ transform.py
@@ -8,57 +8,55 @@ import os
 load_dotenv()
 root = Path(os.getenv('PASTA'))
 
-dia = date.today()
-folder = foot /  f'Deck {dia.strftime("%d.%m.%Y")}'
+day = date.today()
+# Sets path of deck, which is the folder where the files used in the report are taken
+folder = foot /  f'Deck {day.strftime("%d.%m.%Y")}'
 
-
-def tabela_pld():
-    df_pld = pd.read_csv(rf'{pasta}\PLD_relat_d.csv', delimiter=';')
+def pld_table():
+    df_pld = pd.read_csv(folder / f'PLD_relat_d.csv', delimiter=';')
     df_pld1, df_pld2 = df_pld[df_pld['Data'] == df_pld['Data'].iloc[0]], df_pld[df_pld['Data'] == df_pld['Data'].iloc[-1]]
     return df_pld1, df_pld2
 
-def precos_importacao():
-    #Lê arquivo de preços
-    pasta = fr'C:\Users\eduardosilva-aeg\OneDrive - AMBAR ENERGIA LTDA\Documentos\Materiais\Apresentações\01. Janeiro 2026'
-    df_precos = pd.read_csv(rf'{pasta}\precos.csv', delimiter=';')
+def import_prices():
+    #Reads prices file 
+    df_prices = pd.read_csv(folder / 'prices.csv', delimiter=';')
     
-    #Filtra última semana disponível
-    df_precos['dat_iniciovalidade'] = pd.to_datetime(df_precos['dat_iniciovalidade'], format='%Y-%m-%d')
-    hj_preco = df_precos['dat_iniciovalidade'].max()
-    filtro_preco = df_precos[df_precos['dat_iniciovalidade'] == hj_preco]
+    #Gets last available data
+    df_prices['dat_iniciovalidade'] = pd.to_datetime(df_prices['dat_iniciovalidade'], format='%Y-%m-%d')
+    hj_price = df_prices['dat_iniciovalidade'].max()
+    filter_price = df_prices[df_prices['dat_iniciovalidade'] == hj_price]
     
-    #Separa datas de início e fim e remove colunas do df
-    data_inicio, data_fim = hj_preco, hj_preco + pd.Timedelta(days=6)
-    filtro_preco.drop(columns=['dat_iniciovalidade', 'dat_fimvalidade'], inplace=True)
+    #Filters start and end dates
+    start_date, end_date = hj_price, hj_price + pd.Timedelta(days=6)
+    filter_price.drop(columns=['dat_iniciovalidade', 'dat_fimvalidade'], inplace=True)
     
-    #Separa df Argentina e Uruguai
-    preco_ar = filtro_preco[filtro_preco['nom_pais'] == 'ARGENTINA']
-    preco_ur = filtro_preco[filtro_preco['nom_pais'] == 'URUGUAI']
+    #Creates distinct dfs to Argentina and Uruguay
+    price_ar = filter_price[filter_price['nom_pais'] == 'ARGENTINA']
+    price_ur = filter_price[filter_price['nom_pais'] == 'URUGUAI']
 
-    return data_inicio, data_fim, preco_ar, preco_ur
+    return start_date, end_date, price_ar, price_ur
 
-def importacao_blocos():
-    #Lê blocos
-    pasta = fr'C:\Users\eduardosilva-aeg\OneDrive - AMBAR ENERGIA LTDA\Documentos\Materiais\Apresentações\01. Janeiro 2026'
-    df_bloco = pd.read_csv(rf'{pasta}\blocos.csv', delimiter=';')
-    df_bloco[['data', 'hora']] = df_bloco['din_instante'].str.split(' ', expand=True)
-    df_bloco = df_bloco.drop(columns=['din_instante'])
+def import_blocks():
+    #Rwead file
+    df_block = pd.read_csv(folder / 'blocks.csv', delimiter=';')
+    df_block[['data', 'hora']] = df_block['din_instante'].str.split(' ', expand=True)
+    df_block = df_block.drop(columns=['din_instante'])
 
-    #Filtra última data disponível
-    df_bloco['data'] = pd.to_datetime(df_bloco['data'], format='%Y-%m-%d')
-    ultima = df_bloco['data'].max()
-    filtro_bloco = df_bloco[df_bloco['data'] == ultima].drop(columns=['data'])
-    bloco_group = (
-                    filtro_bloco
-                   .groupby(['nom_pais', 'nom_agente', 'nom_bloco'], as_index=False)
+    #Gets last available data
+    df_block['data'] = pd.to_datetime(df_block['data'], format='%Y-%m-%d')
+    last = df_block['data'].max()
+    filter_block = df_block[df_block['data'] == last].drop(columns=['data'])
+    block_group = (
+                    filter_block
+                   .groupby(['nom_pais', 'nom_agente', 'nom_block'], as_index=False)
                    .agg({'val_importacaoprogramada':'mean',
                          'val_importacaodespachada':'mean',
                          'val_importacaoverificada':'mean',
-                         'val_preco':'mean'})
+                         'val_price':'mean'})
                          )
     
     #Separa df Argentina e Uruguai
-    bloco_ar = bloco_group[bloco_group['nom_pais'] == 'ARGENTINA']
-    bloco_ur = bloco_group[bloco_group['nom_pais'] == 'URUGUAI']
+    block_ar = block_group[block_group['nom_pais'] == 'ARGENTINA']
+    block_ur = block_group[block_group['nom_pais'] == 'URUGUAI']
 
-    return ultima, bloco_ar, bloco_ur
+    return last, block_ar, block_ur
