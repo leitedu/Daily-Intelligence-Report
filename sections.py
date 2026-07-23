5b31eca9e3aa4bc25c9a24cd5753cc9c989e3b00
Eduardo Leite <133655512+leitedu@users.noreply.github.com>
2026-07-23 18:25:40 -0300

    Fix parameter names

--- sections.py
+++ sections.py
@@ -3,54 +3,54 @@ from reportlab.lib.units import cm
 from reportlab.lib.enums import TA_JUSTIFY
 
 
-def secao_tabela(titulo, tabela, estilo):
+def table_section(title, table, style):
     return KeepTogether([
-        Paragraph(titulo, estilo['Heading2']),
+        Paragraph(title, style['Heading2']),
         Spacer(1, 0.2*cm),
-        tabela,
+        table,
         Spacer(1, 0.5*cm)
     ])
 
 
-def secao_duas_tabelas(titulo, tabela1, tabela2, estilo):
-    side_by_side = Table([[tabela1, '' , tabela2]], colWidths=[8*cm, 1*cm, 8*cm])
+def double_table_section(title, table1, table2, style):
+    side_by_side = Table([[table1, '' , table2]], colWidths=[8*cm, 1*cm, 8*cm])
 
     side_by_side.setStyle([('VALIGN', (0,0), (-1,-1), 'TOP'),
                            ('LEFTPADDING', (0,0), (-1,-1), 0),
                            ('RIGHTPADDING', (0,0), (-1,-1), 0)])
 
     return KeepTogether([
-        Paragraph(titulo, estilo['Heading2']),
+        Paragraph(title, style['Heading2']),
         Spacer(1, 0.2*cm),
         side_by_side,
         Spacer(1, 0.5*cm)
     ])
 
 
-def secao_imagem(titulo, imagem, estilo, max_width=16*cm):
+def image_section(title, image, style, max_width=16*cm):
     story = []
-    story.append(Paragraph(titulo, estilo['Heading2']))
+    story.append(Paragraph(title, style['Heading2']))
     story.append(Spacer(1, 0.2*cm))
 
-    if imagem.drawWidth > max_width:
-        proporcao = max_width / imagem.drawWidth
-        imagem.drawWidth = max_width
-        imagem.drawHeight = imagem.drawHeight * proporcao
+    if image.drawWidth > max_width:
+        proporcao = max_width / image.drawWidth
+        image.drawWidth = max_width
+        image.drawHeight = image.drawHeight * proporcao
 
-    imagem.hAlign = 'CENTER'
-    story.append(imagem)
+    image.hAlign = 'CENTER'
+    story.append(image)
     story.append(Spacer(1, 0.5*cm))
 
     return story
 
 
-def secao_texto(titulo, texto, estilo):
-    estilo_corpo = estilo['BodyText']
-    estilo_corpo.alignment = TA_JUSTIFY
+def text_section(title, text, style):
+    body_style = style['BodyText']
+    body_style.alignment = TA_JUSTIFY
 
     return [
-        Paragraph(titulo, estilo['Heading2']),
+        Paragraph(title, style['Heading2']),
         Spacer(1, 0.2*cm),
-        Paragraph(texto, estilo_corpo),
+        Paragraph(text, body_style),
         Spacer(1, 0.5*cm)
     ]
