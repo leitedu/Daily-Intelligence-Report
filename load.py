9fef4d61193780b28cbddf34ca87ccc62d56e1f5
Eduardo Leite <133655512+leitedu@users.noreply.github.com>
2026-07-23 18:13:43 -0300

    Refactor load_content function and update data sources

--- load.py
+++ load.py
@@ -9,73 +9,74 @@ import os
 load_dotenv()
 root = Path(os.getenv('PASTA'))
 
-dia = date.today()
-folder = root /  f'Deck {dia.strftime("%d.%m.%Y")}'
+# Sets path of deck, where the files used in the report are taken
+day = date.today()
+folder = root /  f'Deck {day.strftime("%d.%m.%Y")}'
 
 def load_content():
 
     #1 Marginal System Cost (CMO) in Brazil
-    cmo_barra = df_cmo_barra()
+    cmo = df_cmo()
     #2 Electricity Spot Price (PLD) in Brazil
-    pld = tabela_pld()
+    pld = pld_table()
     #3 Marginal System Cost (CMO) in Argentina
     cmo_arg = pd.read_csv(folder / f'CMO_arg.csv', delimiter=',')
     #4 Exchange Electricity Prices by Submarket in Brazil 
     bbce = pd.read_csv(folder / f'marcacao_bbce.csv', delimiter=';')
     #5 Natural gas Prices
-    pg_usinas = pd.read_csv(folder / f'pg_usinas.csv', delimiter=';')
+    gas_prices = pd.read_csv(folder / f'gas_prices.csv', delimiter=';')
     #6 Thermal dispatch in the day - Brazil
-    despacho_br = folder / f'despacho_termico_brasil.png'
-    #7 Biggest Variable Unitary Costs of thermal plants in Brazil, by submarket 
+    br_dispatch = folder / f'despacho_termico_brasil.png'
+    #7 Highest Variable Unitary Costs of thermal plants in Brazil, by submarket 
     m_cvus = pd.read_csv(folder / f'maiores_cvus.csv', delimiter=';')
     #8 Stored energy in hidric reservoirs
     ena = folder / f'ENA.png'
     #9 Electricity exchanges between Brazil, Argentina and Uruguay (Volume, Prices and Offers)
-    intercambios = folder / f'intercambios.png'
-    inicio_imp_preco, fim_imp_preco, preco_ar, preco_ur = precos_importacao()
-    data_blocos, bloco_ar, bloco_ur = importacao_blocos()
+    exchanges = folder / f'exchanges.png'
+    start_imp_price, end_imp_price, price_ar, price_ur = import_prices()
+    data_blocks, block_ar, block_ur = import_blocks()
     
 
     return [{'type': 'table',
-             'title': 'CMO na barra por usina (R$/MWh)',
-             'data': cmo_barra},
+             'title': 'CMO (R$/MWh)',
+             'data': cmo},
 
             {'type': 'two_tables',
              'title': 'PLD (R$/MWh)',
              'data': pld},
 
              {'type': 'table',
-             'title': 'CMO Semanal Argentina (US$/MWh)',
+             'title': 'Weekly Argentine CMO (US$/MWh)',
              'data': cmo_arg},
 
              {'type': 'table',
-             'title': 'Marcação de Preços BBCE por submercado (R$/MWh)',
+             'title': 'BBCE prices (R$/MWh)',
              'data': bbce},
 
              {'type': 'table',
-             'title': 'Preços de gás (R$/m³)',
-             'data': pg_usinas},
+             'title': 'Natural gas prices (R$/m³)',
+             'data': gas_prices},
              
              {'type': 'table',
-             'title': 'Maiores CVUs despachados (R$/MWh)',
+             'title': 'Highest dispatched CVUs (R$/MWh)',
              'data': m_cvus},
 
              {'type': 'image',
-             'title': 'Despacho térmico Brasil',
-             'data': despacho_br},
+             'title': 'Thermal dispatch Brazil',
+             'data': br_dispatch},
 
              {'type': 'image',
              'title': 'ENA',
              'data': ena},
             
              {'type': 'two_tables',
-             'title': f'Preços de importação por bloco {inicio_imp_preco.strftime("%d.%m.%Y")} a {fim_imp_preco.strftime("%d.%m.%Y")} (R$/MWh)',
-             'data': [preco_ar, preco_ur]},
+             'title': f'Import prices by Block {start_imp_price.strftime("%d.%m.%Y")} to {end_imp_price.strftime("%d.%m.%Y")} (R$/MWh)',
+             'data': [price_ar, price_ur]},
 
              {'type': 'two_tables',
-             'title': f'Importação por bloco {data_blocos.strftime("%d.%m.%Y")} (MW)',
-             'data': [bloco_ar, bloco_ur]},
+             'title': f'Imported volume by Block {data_blocks.strftime("%d.%m.%Y")} (MW)',
+             'data': [block_ar, block_ur]},
             
-             {'type': 'image', 'title': 'Intercâmbios de energia internacionais', 'data': intercambios},
+             {'type': 'image', 'title': 'Iinternational energy exchanges', 'data': exchanges},
 
              ]
